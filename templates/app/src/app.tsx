8c7691fc8e6ed8604ed3f152d2a137606d1dd211
vuuvv <vuuvv@qq.com>
2018-12-04 16:25:03 +0800

    formated app.tsx

--- templates/app/src/app.tsx
+++ templates/app/src/app.tsx
@@ -1,10 +1,11 @@
 <% if (props.react.includes('dva')) { %>
-  export const dva = {
-    config: {
-      onError(err) {
-        err.preventDefault();
-        console.error(err.message);
-      },
+export const dva = {
+  config: {
+    onError(err) {
+      err.preventDefault();
+      console.error(err.message);
     },
-  };
+  },
+};
+
 <% } %>
